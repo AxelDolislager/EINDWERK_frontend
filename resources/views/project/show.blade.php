3453be598420c5d391a81533f9f3ae7d36b0c8bf
Axel D'Olislager <axel.dolislager@gmail.com>
2019-06-07 19:06:58 +0200

    da besta toch?

--- resources/views/project/show.blade.php
+++ resources/views/project/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('content')
-{{dd($project->relationships)}}
+{{dd($project)}}
     <section class='todolist'>
         @foreach($project->relationships->todos->data as $todo)
             <form action='/project/{{$project->id}}/todo/{{$todo->id}}'>
