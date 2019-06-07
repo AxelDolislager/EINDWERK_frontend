07271f2fc25e6b059f0583185854e38bba233e9e
Axel D'Olislager <axel.dolislager@gmail.com>
2019-06-07 18:32:37 +0200

    styling output!

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -3,15 +3,30 @@
 @section('content')
     <section class='projectlist'>
         @foreach($projects as $project)
+            <a href='/project/{{ $project->id }}' style='background-color: green;'>
+                <div class='left'>
+                    <span class='projecttitle'>{{ $project->attributes->title }}</span>
+                    <span class='projecttotals'>0 / ? completed</span>
+                </div>
+                <div class='right'>
+                    <div class='projectdate'>
+                        <span>created at</span>
+                        <span class='projectdatavalue'>
+                            20/05/2019
+                        </span>
+                    </div>
+                </div>
+            </div>
+
+
+
             {{dd($project->server)}}
             {{dd($project->type)}}
             {{dd($project->id)}}
+            {{dd($project->attributes->title)}}
+            {{dd($project->attributes->body)}}
         @endforeach
-        {{-- @foreach($projects['data'] as $project => $a)
-            {{dd($project)}}
-            {{dd($a)}}
-        @endforeach --}}
-        <a href='/project/id' style='background-color: #e74c3c;'>
+        {{-- <a href='/project/id' style='background-color: #e74c3c;'>
             <div class='left'>
                 <span class='projecttitle'>Project 1</span>
                 <span class='projecttotals'>0 / 5 completed</span>
@@ -39,7 +54,7 @@
                     </span>
                 </div>
             </div>
-        </a>
+        </a> --}}
     </section>
     <div class='bottomaddoverlay'>
         <a href='/project/create' class='addbutton'><i class='fal fa-plus'></i></a>
