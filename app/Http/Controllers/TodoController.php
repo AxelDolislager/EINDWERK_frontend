5535ac3ba3d225fbbb7c89a5dc666cf8856bbaef
Axel D'Olislager <axel.dolislager@gmail.com>
2019-06-08 20:08:03 +0200

    create-todo

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,16 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class TodoController extends Controller{
     public function index(){}
     public function create($projectid){
         return view('todo.create', [
             "pagetitle" =>  "Nieuwe taak aanmaken",
-            "previous_page" => "/project/$projectid"
+            "previous_page" => "/project/$projectid",
+            "project_id" => $projectid
         ]);
     }
-    public function store(Request $request){}
+    public function store(Request $request){
+        $client = new Client(['base_uri' => env('BACKEND_SERVER_URL')]);
+        $response = $client->request('POST', 'todos', [
+            'form_params' => [
+                'project_id' => $request->project_id,
+                'title' => $request->title,
+            ],
+        ]);
+        return redirect('/project/' . $request->project_id);
+    }
     public function show($id){}
     public function edit($id){}
     public function update(Request $request, $id){}
--- resources/views/todo/create.blade.php
+++ resources/views/todo/create.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
     <section class='createtodo'>
-        <form class='createtodoform' action='/todo/' method='post'>
+        <form class='createtodoform' action='/project/{{$project_id}}/todo/' method='post'>
             @csrf
             <h4>Nieuwe todo aanmaken...</h4>
-            <input type='hidden' name='project_id' value='projectid'>
-            <input type='text' class='input' value='Todonaam' name='title'>
+            <input type='hidden' name='project_id' value='{{$project_id}}'>
+            <input type='text' class='input' placeholder='Todonaam' name='title'>
             <input type='submit' id='submit' value='Maak todo aan'>
         </form>
     </section>
